dd2a3fa4f92a6286a0d1672a8e5245afbae44e3c
nitin-das <nitin.das@thomson.co.uk>
2020-02-14 19:05:46 +0000

    Modified code

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "endpoint" {
-  value = aws_rds_cluster.market_rds_cluster.endpoint
+  value = aws_rds_cluster.rds_cluster.endpoint
 }
 
 output "id" {
-  value = aws_rds_cluster.market_rds_cluster.id
+  value = aws_rds_cluster.rds_cluster.id
 }
 
 output "arn" {
-  value = aws_rds_cluster.market_rds_cluster.arn
+  value = aws_rds_cluster.rds_cluster.arn
 }
 
 output "database_name" {
-  value = aws_rds_cluster.market_rds_cluster.database_name
+  value = aws_rds_cluster.rds_cluster.database_name
 }
 
 output "engine" {
-  value = aws_rds_cluster.market_rds_cluster.engine
+  value = aws_rds_cluster.rds_cluster.engine
 }
 
 output "master_username" {
-  value = aws_rds_cluster.market_rds_cluster.master_username
+  value = aws_rds_cluster.rds_cluster.master_username
 }
 
